f01e527787ec1ca4703fe9fc22f69ffa72d82a37
njugunamwangi <njugunamwangi.n@gmail.com>
2024-09-11 00:46:36 +0300

    Install wireui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js")
+    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: colors.emerald,
+                secondary: colors.gray,
+                positive: colors.green,
+                negative: colors.red,
+                warning: colors.amber,
+                info: colors.blue
             },
         },
     },
